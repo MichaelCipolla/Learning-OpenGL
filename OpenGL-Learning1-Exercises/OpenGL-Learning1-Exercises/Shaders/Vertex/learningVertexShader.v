9a3e3bfb3668efa6bca3e34c260f2ac06b7a066a
MichaelCipolla <michael.cipolla.eemc@gmail.com>
2022-04-10 21:53:01 -0700

    Learning Shaders - Finished exercise 3. I believe the reason for the black is because we are passing a vector position which can have negative values / 0 values. Since 0 on the color vector.x component is no color this displays as black

--- OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Shaders/Vertex/learningVertexShader.v
+++ OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Shaders/Vertex/learningVertexShader.v
@@ -3,9 +3,11 @@ layout(location = 0) in vec3 aPos; // "position" has attribute position 0
 layout(location = 1) in vec3 aColor; // "color" has attribute position 1
 
 out vec3 ourColor;
+out vec4 ourPosition;
 uniform float offset;
 
 void main() {
     gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
     ourColor = aColor; // output color
+    ourPosition = gl_Position; // output position
 }
