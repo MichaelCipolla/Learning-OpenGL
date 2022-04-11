8cd99e2073bb513217220f90603f54af8d8020e3
MichaelCipolla <michael.cipolla.eemc@gmail.com>
2022-04-10 21:48:03 -0700

    Learning Shaders - Finished exercise 2. The triangle will now move using a uniform float that was defined in the vertex shader

--- OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Shaders/Vertex/learningVertexShader.v
+++ OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Shaders/Vertex/learningVertexShader.v
@@ -3,9 +3,9 @@ layout(location = 0) in vec3 aPos; // "position" has attribute position 0
 layout(location = 1) in vec3 aColor; // "color" has attribute position 1
 
 out vec3 ourColor;
-uniform vec1 offset;
+uniform float offset;
 
 void main() {
-    gl_Position = vec4(aPos.x + offset.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
     ourColor = aColor; // output color
 }
