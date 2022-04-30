e7ce9af250a9e1fabefe35a9248df2370124c06b
MichaelCipolla <michael.cipolla.eemc@gmail.com>
2022-04-30 11:11:23 -0700

    LearningShaders - Added gitignore to remove Visual Studio specific files - Fixed texture mapping bug - Updated shaders to allow for the mixing of two different textures

--- OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Render.cpp
+++ OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Render.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include "Shaders.h"
+#include "stb_image.h"
 
 ////shaders (RGBA)
 //const char* vertexShader = "#version 330 core\n"
@@ -101,45 +102,103 @@ int main() {
 	unsigned int shaderProgram = shaderLoader->createShaderProgram("Shaders/Vertex/learningVertexShader.v", "Shaders/Fragment/learningFragmentShader.f");
 
 	float vertices[] = {
-		 0.0f, 0.5f, 0.0f, // pos...
+		 0.5f, 0.5f, 0.0f, // pos...
 		 1.0f, 0.0f, 0.0f, // color...
-
-		 -0.5f, -0.5f, 0.0f,
-		 0.0f, 1.0f, 0.0f,
+		 1.0f, 1.0f, //texture coords
 
 		 0.5f, -0.5f, 0.0f,
+		 0.0f, 1.0f, 0.0f,
+		 1.0, 0.0f,
+
+		 -0.5f, -0.5f, 0.0f,
 		 0.0f, 0.0f, 1.0f,
+		 0.0f, 0.0f,
+
+		  -0.5f, 0.5f, 0.0f,
+		 1.0f, 1.0f, 0.0f,
+		 0.0f, 1.0f,
+
 	};
 
-	int vertexOrdering[] = {
+	int vertexOrdering1[] = {
 		0, 1, 2,
-		2, 3, 4
+		0, 2, 3,
 	};
 
 	unsigned int VAO1; // This stores vertexAttribute calls 
 	glGenVertexArrays(1, &VAO1);
 	glBindVertexArray(VAO1);
-	
 
 	unsigned int VBO1; // This stores vertex information
 	glGenBuffers(1, &VBO1);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO1);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0); // vertexAttribute pointers can only be initialized after a VBO is defined
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	int vertexOrdering1[] = {
-		0, 1, 2,
-	};
-
 	unsigned int EBO1;
 	glGenBuffers(1, &EBO1);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO1);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(vertexOrdering1), vertexOrdering1, GL_STATIC_DRAW);
 
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // vertexAttribute pointers can only be initialized after a VBO is defined
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	// set the texture wrapping/filtering options (on currently bound texture)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	// set the texture wrapping/filtering options (on currently bound texture)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load and generate the texture
+	
+	data = stbi_load("Textures/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	/*glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);*/
+	stbi_image_free(data);
+
+
+	//std::cout << glGetError() << std::endl;
+	shaderLoader->use();
+	shaderLoader->setInt("texture1", 0); //set which GL_TEXTUREX this texture is associated with
+	shaderLoader->setInt("texture2", 1);
 
 	while (!glfwWindowShouldClose(window)) {
 		// input
@@ -148,18 +207,21 @@ int main() {
 		// rendering commands
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		
+		/*
 		float timeValue = glfwGetTime();
 		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 
 		shaderLoader->use();
-		shaderLoader->setFloat("offset", greenValue);
-
+		shaderLoader->setFloat("offset", greenValue);*/
+		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		//EBO method:
 		//glUseProgram(shaderProgram);
 		glBindVertexArray(VAO1);
-		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
 		// check and call events and swap the buffers
--- OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Shaders/Vertex/learningVertexShader.v
+++ OpenGL-Learning1-Exercises/OpenGL-Learning1-Exercises/Shaders/Vertex/learningVertexShader.v
@@ -1,13 +1,13 @@
 #version 330 core
 layout(location = 0) in vec3 aPos; // "position" has attribute position 0
 layout(location = 1) in vec3 aColor; // "color" has attribute position 1
+layout(location = 2) in vec2 aTexCoord;
 
 out vec3 ourColor;
-out vec4 ourPosition;
-uniform float offset;
+out vec2 TexCoord;
 
 void main() {
-    gl_Position = vec4(aPos.x + offset, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos, 1.0);
     ourColor = aColor; // output color
-    ourPosition = gl_Position; // output position
+    TexCoord = aTexCoord;
 }
